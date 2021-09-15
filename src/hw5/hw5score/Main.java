f4d45fddff1f52bd8001b5865517bb6ae7f6f209
ebrahimi76 <ebrahimimohammadali76@gmail.com>
2021-09-15 10:56:01 +0430

    hw5 commit

---
+++ src/hw5/hw5score/Main.java
@@ -0,0 +1,9 @@
+package hw5.hw5score;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TravelCostCal calculator = new TravelCostCal();
+        calculator.travelCostCal();
+    }
+}
