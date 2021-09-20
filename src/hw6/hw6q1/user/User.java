5e4011d237fd3659663f3433d6de58ce254dab10
ebrahimi76 <ebrahimimohammadali76@gmail.com>
2021-09-20 22:22:06 +0430

    add user class.

--- src/hw6/hw6q1/user/User.java
+++ src/hw6/hw6q1/user/User.java
@@ -2,11 +2,8 @@ package hw6.hw6q1.user;
 
 import hw6.hw6q1.Basket;
 
-import java.util.Scanner;
-
 public class User {
 
-    private Scanner input = new Scanner(System.in);
     private String username;
     private String password;
     private String firstName;
@@ -15,69 +12,5 @@ public class User {
     private String emailAddress;
     private Address address;
     private Basket basket;
-
-    public Scanner getInput() {
-        return input;
-    }
-
-    public void setInput(Scanner input) {
-        this.input = input;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(int mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
 
 }
