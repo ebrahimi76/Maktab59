4d60af83b9b7a53e234a9bf8a81782c7a467fc2a
ebrahimi76 <ebrahimimohammadali76@gmail.com>
2021-09-20 23:47:55 +0430

    Refactor classes

--- src/hw6/hw6q1/Site.java
+++ src/hw6/hw6q1/Site.java
@@ -1,12 +1,11 @@
 package hw6.hw6q1;
 
 import hw6.hw6q1.site.Products;
+import hw6.hw6q1.site.Store;
 
 public class Site {
     public static void main(String[] args) {
-        Products products = new Products();
-        products.setProducts();
-        System.out.println(products);
-
+        Store store = new Store();
+        store.run();
     }
 }
--- src/hw6/hw6q1/user/User.java
+++ src/hw6/hw6q1/user/User.java
@@ -5,77 +5,95 @@ import hw6.hw6q1.site.Basket;
 import java.util.Scanner;
 
 public class User {
-
-    private Scanner input = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
     private String username;
     private String password;
     private String firstName;
     private String lastName;
     private int mobileNumber;
     private String emailAddress;
-    private Address address;
+    private Address address = new Address();
     private Basket basket;
+
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername() {
-        System.out.println("Enter userName: ");
-        username = input.next();
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword() {
-        System.out.println("Enter password: ");
-        password = input.next();
-        ;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName() {
-        System.out.println("Enter firstName: ");
-        firstName = input.next();
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName() {
-        System.out.println("Enter lastName: ");
-        lastName = input.next();
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public int getMobileNumber() {
         return mobileNumber;
     }
 
-    public void setMobileNumber() {
-        System.out.println("Enter mobileNumber: ");
-        mobileNumber = input.nextInt();
+    public void setMobileNumber(int mobileNumber) {
+        this.mobileNumber = mobileNumber;
     }
 
     public String getEmailAddress() {
         return emailAddress;
     }
 
-    public void setEmailAddress() {
-        System.out.println("Enter emailAddress: ");
-        emailAddress = input.next();
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
     }
 
-    public void getAddress() {
-        System.out.println(address);
+    public Address getAddress() {
+        return address;
     }
 
-    public void setAddress() {
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Basket getBasket() {
+        return basket;
+    }
+
+    public void setBasket(Basket basket) {
+        this.basket = basket;
+    }
+
+    public void setUser() {
+        System.out.println("Enter Username :");
+        this.setUsername(scanner.next());
+        System.out.println("Enter Password :");
+        this.setPassword(scanner.next());
+        System.out.println("Enter first name :");
+        this.setFirstName(scanner.next());
+        System.out.println("Enter last name :");
+        this.setLastName(scanner.next());
+        System.out.println("Enter Phone :");
+        this.setMobileNumber(scanner.nextInt());
+        scanner.next();
+        System.out.println("Enter Email :");
+        this.setEmailAddress(scanner.next());
         address.setAddress();
     }
 }
